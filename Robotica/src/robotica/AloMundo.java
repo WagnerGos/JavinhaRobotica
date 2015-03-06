a9e4231790fafa4ecdf2bf6a048a24b7e73b2489
Wagner Gonçalves <wagner.gos12345@gmail.com>
2015-03-05 23:59:57 -0300

    v1

---
+++ Robotica/src/robotica/AloMundo.java
@@ -0,0 +1,16 @@
+package robotica;
+
+/**
+ *
+ * @author Wagner
+ * Exercício 1
+ */
+public class AloMundo {
+
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Alo Mundo");
+    }
+    
+}
